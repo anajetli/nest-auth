1a322c65d2a8f9cdc88a62c44805f2d0b7e08309
unknown <atif@plego.com>
2023-12-12 03:49:24 +0500

    29-Repository Pattern

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DB_Event } from './event.entity';
 import { EventsController } from './events.controller';
 
 @Module({
@@ -12,7 +13,8 @@ import { EventsController } from './events.controller';
       port: 3307,
       username: 'root',
       password: '',
-      database: 'esco'
+      database: 'esco',
+      entities: [DB_Event]
     })
   ],
   controllers: [AppController, EventsController],
--- src/event.entity.ts
+++ src/event.entity.ts
@@ -1,7 +1,18 @@
-export class Event {
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+@Entity('events')
+export class DB_Event {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     description: string;
+
+    @Column()
     when: Date;
+
+    @Column()
     address: string;
 }
