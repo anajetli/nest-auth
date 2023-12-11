abc424869bf64bc7d67d8fbacabe0eaaae004d79
unknown <atif@plego.com>
2023-12-12 03:41:45 +0500

    26-Connecting to the DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './events.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'root',
+      password: '',
+      database: 'esco'
+    })
+  ],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
